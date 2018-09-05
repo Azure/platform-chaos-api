4e7338c5f3042cd0430213a8bf5b9f42d6179ada
Allison Durkan <allison.durkan@microsoft.com>
2018-09-05 17:02:22 -0400

    added mock auth, basic testing

--- test/basic.js
+++ test/basic.js
@@ -1,26 +1,141 @@
 const request = require('supertest');
-const express = require('express');
+const fs = require('fs');
+const expect = require('chai').expect
+const server = require('../lib/app');
 
-const app = express(); 
+const app = server({ isProd: false}); 
 
-app.get('/extensions', function(req, res) {
-    res.status(200).json({ name: 'test', desc: 'test desc', uri: 'https://test.com' });
+const extensionsPath = `${__dirname}/../webapp-extension-store.json`;
+
+describe('GET /extensions when there are no extensions', function() {
+
+    before(function() {
+        if (fs.existsSync(extensionsPath)) {
+            fs.unlink(extensionsPath, function(error) {
+                if (error) {
+                    throw error;
+                }
+                console.log('Deleted extension store.');
+            })
+        }
+    })
+
+    it('should require authorization', function(done) {
+        request(app)
+            .get('/extensions')
+            .expect(401)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            })
+    })
+
+    it('responds with all registered extensions as an array', function(done) {
+        request(app)
+            .get('/extensions')
+            .set('Authorization', 'bearer ' + 'mock-token')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                expect(res.body)
+                    .to.be.an.instanceof(Array)
+                    .and.to.have.length(0)
+                done()
+            })
+    })
 });
 
-request(app)
-    .get('/user')
-    .expect('Content-Type', /json/)
-    .expect('Content-Length', '15')
-    .expect(200)
-    .end(function(err, res) {
-        if (err) throw err;
-});
+describe('POST /extensions', function() {
 
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+    before(function() {
+        if (fs.existsSync(extensionsPath)) {
+            fs.unlink(extensionsPath, function(error) {
+                if (error) {
+                    throw error;
+                }
+                console.log('Deleted extension store.');
+            })
+        }
+    })
+
+    var body = {
+        "name": "test name",
+        "uri": "https://test.com",
+        "desc": "test desc"
+    };
+
+    var body2 = {
+        "name": "test name2",
+        "uri": "https://test.com2",
+        "desc": "test desc2"
+    };
+
+    it('should require authorization', function(done) {
+        request(app)
+            .post('/extensions')
+            .send(body)
+            .expect(401)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            })
+    })
+
+    it('creates a new extension with payload posted', function(done) {
+        request(app)
+            .post('/extensions')
+            .set('Authorization', 'bearer ' + 'mock-token')
+            .set('Content-Type', 'application/json')
+            .send(body)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done()
+            })
+    })
+
+    it('can get the extension that just registered', function(done) {
+        request(app)
+            .get('/extensions')
+            .set('Authorization', 'bearer ' + 'mock-token')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                expect(res.body)
+                    .to.be.an.instanceof(Array)
+                    .and.to.have.length(1)
+                expect(res.body[0])
+                    .to.contain.keys('name', 'uri', 'desc')
+                done()
+            })
+    })
+
+    it('creates a new extension with payload posted', function(done) {
+        request(app)
+            .post('/extensions')
+            .set('Authorization', 'bearer ' + 'mock-token')
+            .set('Content-Type', 'application/json')
+            .send(body2)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done()
+            })
+    })
+
+    it('can get the extension that just registered', function(done) {
+        request(app)
+            .get('/extensions')
+            .set('Authorization', 'bearer ' + 'mock-token')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                expect(res.body)
+                    .to.be.an.instanceof(Array)
+                    .and.to.have.length(2)
+                expect(res.body[0])
+                    .to.contain.keys('name', 'uri', 'desc')
+                done()
+            })
+    })
+})
